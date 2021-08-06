41ea12e0792d40495fae96b612945a929a76d2e2
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-08-06 18:32:30 +0900

    textarea 초기화

--- javaScript/question.js
+++ javaScript/question.js
@@ -5,6 +5,7 @@ function GoMain() {
 function NextQuestion() {
     document.getElementById("Q1").innerHTML="중요한 내용이나 기억해야 할 내용이 있나요?";
     document.querySelector(".nextBtn").className="GoMain"
+    document.querySelector("textarea").value=""
     document.querySelector(".GoMain").addEventListener("click", GoMain);
 }
 
